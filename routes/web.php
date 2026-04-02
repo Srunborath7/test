c4750cf6b03673147d8e832527335d68083a5667
Srunborath7 <srunborath7@gmail.com>
2026-04-02 22:39:04 +0700

    y

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('admin/home_page');
-// });
-Route::get("/",[UserController::class,"index"]);
-Route::post("/login",[UserController::class,"authLogin"]);
-Route::get('/login',function(){
-    return View("auth/login");
+Route::get('/', function () {
+    return "Deploy with Render.com";
 });
+// Route::get("/",[UserController::class,"index"]);
+// Route::post("/login",[UserController::class,"authLogin"]);
+// Route::get('/login',function(){
+//     return View("auth/login");
+// });
 
