c5381be2f0e11a638dd5270b062ea94b69e72beb
Srunborath7 <srunborath7@gmail.com>
2026-04-03 22:03:03 +0700

    update url

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,11 +9,15 @@ class Product extends Model
     protected $collection = 'products';
     protected $fillable = ['name', 'image', 'price', "discount",];
     protected $appends = ['image_url'];
+    // public function getImageUrlAttribute()
+    // {
+    //     if (!empty($this->image)) {
+    //         return asset('storage/' . $this->image);
+    //     }
+    //     return null;
+    // }
     public function getImageUrlAttribute()
-    {
-        if (!empty($this->image)) {
-            return asset('storage/' . $this->image);
-        }
-        return null;
-    }
+{
+    return secure_url('storage/' . $this->image);
+}
 }
